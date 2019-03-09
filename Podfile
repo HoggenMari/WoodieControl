a4c49199b0236f09212230354d0db7d821574f0c
HoggenMari <marius.hoggenmueller@campus.lmu.de>
2019-03-10 03:09:36 +1100

    * setup project * mqtt connection

---
+++ Podfile
@@ -0,0 +1,22 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'WoodieControl' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for WoodieControl
+  pod 'SwiftSocket'
+  pod 'CocoaMQTT'
+  
+  target 'WoodieControlTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'WoodieControlUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
--- WoodieControl/ViewController.swift
+++ WoodieControl/ViewController.swift
@@ -7,14 +7,45 @@
 //
 
 import UIKit
+import SwiftSocket
+import CocoaMQTT
 
 class ViewController: UIViewController {
+
+    let mqttClient = CocoaMQTT(clientID: "iOS Device", host: "192.168.0.102", port: 6667)
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let client = TCPClient(address: "192.168.0.102", port: 9000)
+        switch client.connect(timeout: 1) {
+        case .success:
+            switch client.send(string: "Blaaaaaa" ) {
+            case .success:
+                guard let data = client.read(1024*10) else { return }
+                
+                if let response = String(bytes: data, encoding: .utf8) {
+                    print(response)
+                }
+            case .failure(let error):
+                print(error)
+            }
+        case .failure(let error):
+            print(error)
+        }
     }
 
-
+    @IBAction func connect(_ sender: Any) {
+        mqttClient.connect()
+    }
+    
+    @IBAction func disconnect(_ sender: Any) {
+        mqttClient.disconnect()
+    }
+    
+    @IBAction func reboot(_ sender: Any) {
+        mqttClient.publish("rpi/reboot", withString: "true")
+    }
 }
 
